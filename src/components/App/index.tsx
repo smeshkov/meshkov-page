4e8245e110fe9feea61d40ff243f4208baa5f1d4
Sergey Meshkov <smeshkow@gmail.com>
2023-03-07 13:41:19 +1100

    Layout beggining

--- src/components/App/index.tsx
+++ src/components/App/index.tsx
@@ -2,7 +2,72 @@ import React from 'react';
 
 const App = (): JSX.Element => {
     return (
-        <>Hello world</>
+        <>
+            <nav className="navbar navbar-inverse">
+                <div className="container-fluid">
+                    {/* <div className="row">
+                <div className="col-md-12">
+                    <p className="text-center">Hello world</p>
+                </div>
+            </div> */}
+                    <div className="navbar-header">
+                        <button type="button" className="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
+                            <span className="sr-only">Toggle navigation</span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                            <span className="icon-bar"></span>
+                        </button>
+                        <a className="navbar-brand" href="#">Brand</a>
+                    </div>
+
+                    {/* <!-- Collect the nav links, forms, and other content for toggling --> */}
+                    <div className="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+                        <ul className="nav navbar-nav">
+                            <li className="active"><a href="#">Link <span className="sr-only">(current)</span></a></li>
+                            <li><a href="#">Link</a></li>
+                            <li className="dropdown">
+                                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                                <ul className="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li role="separator" className="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                    <li role="separator" className="divider"></li>
+                                    <li><a href="#">One more separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                        <form className="navbar-form navbar-left">
+                            <div className="form-group">
+                                <input type="text" className="form-control" placeholder="Search" />
+                            </div>
+                            <button type="submit" className="btn btn-default">Submit</button>
+                        </form>
+                        <ul className="nav navbar-nav navbar-right">
+                            <li><a href="#">Link</a></li>
+                            <li className="dropdown">
+                                <a href="#" className="dropdown-toggle" data-toggle="dropdown" role="button" aria-haspopup="true" aria-expanded="false">Dropdown <span className="caret"></span></a>
+                                <ul className="dropdown-menu">
+                                    <li><a href="#">Action</a></li>
+                                    <li><a href="#">Another action</a></li>
+                                    <li><a href="#">Something else here</a></li>
+                                    <li role="separator" className="divider"></li>
+                                    <li><a href="#">Separated link</a></li>
+                                </ul>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+            </nav>
+            <div className="container-fluid">
+                <div className="jumbotron">
+                    <h1>Hello, world!</h1>
+                    <p>This is a simple hero unit, a simple jumbotron-style component for calling extra attention to featured content or information.</p>
+                    <p><a className="btn btn-primary btn-lg" href="#" role="button">Learn more</a></p>
+                </div>
+            </div>
+        </>
     );
 }
 
